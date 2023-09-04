1c6764c77a080a66ee153f6c6029a7ed8b9750b9
AGNES LILY <97900099+AgnesLily2772@users.noreply.github.com>
2023-09-04 23:48:00 +0530

    Add files via upload

---
+++ Model/Model.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose"
+
+const userScheme = mongoose.Schema({
+    username:{type:String,required:true,unique:true},
+    email:{type:String},
+    password:{type:String,required:true},
+    createdAt: {type: Date,default: new Date()},
+    avatarImage:{type:String}
+})
+export const UserModel = mongoose.model("User",userScheme)
+
+const messageSchema = mongoose.Schema({
+    uniqueId:{type:String},
+    messages:[{
+        sender:{type:String},
+        receiver:{type:String},
+        text:{type:String},
+        date: { type: Date, default: Date.now }, // Use Date.now to automatically generate the current date
+        time: { type: String, default: function() {
+            const now = new Date();
+            const hours = String(now.getHours()).padStart(2, '0');
+            const minutes = String(now.getMinutes()).padStart(2, '0');
+            return `${hours}:${minutes}`;
+        }},
+    }],
+})
+export const MsgModel = mongoose.model("Message",messageSchema)
+/*
+Messages{
+    id = "from"+"Send"
+    message[
+        data:
+        timestamp:
+        time:
+    ]
+}
+*/
---
+++ index.js
@@ -0,0 +1,50 @@
+import express from "express"
+import mongoose from "mongoose"
+import cors from "cors"
+import userRoutes from "./Routes/Routes.js"
+
+import {Server, Socket} from 'socket.io'
+import dotenv from "dotenv"
+dotenv.config()
+
+const DB_URL = process.env.DB_URL
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json())
+
+app.use(cors())
+app.use("/api/user",userRoutes)
+
+mongoose.connect(DB_URL,  {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      })
+.then(()=> console.log("DB connected"))
+.catch((error) => console.log("Error occured",error))
+
+const server = app.listen(PORT,()=>console.log(`Port is running on PORT: ${PORT}`))
+
+const socket = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true
+    }
+});
+
+const onlineUsers = new Map();
+
+socket.on("connection", (socket) => {
+    socket.on("add-user", (userId) => {
+        if(userId){
+            onlineUsers.set(userId, socket.id);
+        }
+    });
+
+    socket.on("message-send", (data) => {
+        const sendUserSocket = onlineUsers.get(data.to_id)
+        if(sendUserSocket){
+            socket.to(sendUserSocket).emit("message-recieve",data)
+        }
+    });
+});
